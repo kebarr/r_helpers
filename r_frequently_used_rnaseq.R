bfd8b12b77371afc500346d119b4295f570ad576
Katharine Barr <mbbxhkb2@B-LOSX349HV2H.local>
2020-01-15 10:35:23 +0000

    initial commit

---
+++ r_frequently_used_rnaseq.R
@@ -0,0 +1,14 @@
+library("ggplot2")
+library("RColorBrewer")
+library("gplots")
+library("ggdendro")
+library("ggrepel")
+library("DESeq2")
+library("readxl")
+library("genefilter")
+library("pcaExplorer")
+
+
+setwd("~/Documents/....")
+
+
